f678df05a77be8b3f6947321594a00c6dd29630e
Jeffery Patterson <jpatterson933@ucla.edu>
2021-03-26 12:10:49 -0700

    Products API
    Added products api grab folder which is only used for grabbing products. I also created a seed file for our products. I only added 6 products. Unfortunately they are of the same brand and company. However, I will make sure to diversify the products going forward but in the meantime will use these six products to create our model for products after lunch.

---
+++ controllers/api/products-api-grab.js
@@ -0,0 +1,35 @@
+//------------------------USED FOR GRABBING PRODUCTS --- NO ASSOCIATED ROUTES YET--------------------//
+
+const axios = require('axios').default;
+
+// axios.get('https://api.github.com/orgs/axios')
+//   .then(response => {
+//     console.log(response.data);
+//   }, error => {
+//     console.log(error);
+//   });
+
+//an object 
+const options = {
+    method: 'GET',
+    url: 'https://neobi.p.rapidapi.com/api/products',
+    params: {rev: 'v1.9', count: '50', htmlDescription: '0', page: '1'},
+    headers: {
+      'x-rapidapi-key': '3ae9d6acaemshb175628a0959ccfp18deb7jsn8c86242e0312',
+      'x-rapidapi-host': 'neobi.p.rapidapi.com'
+    }
+  };
+  
+  axios.request(options).then(function (response) {
+
+        for (let i = 0; i < response.data.length; i++) {
+          console.log()
+        
+       
+      }
+      console.log(response.data)
+    
+    //   console.log(response.data);
+  }).catch(function (error) {
+      console.error(error);
+  });
