4393947aeb3765f29c9e4b656b3c6f0fee1e3c1c
Jennifer Vereecken <77600929+jbeans12@users.noreply.github.com>
2021-03-26 14:09:58 -0700

    handlebars/public

---
+++ public/js/login.js
@@ -0,0 +1,50 @@
+const loginForm = async (event) => {
+    event.preventDefault();
+
+    const email = document.querySelector('#emailLogin').value.trim();
+    const password  = document.querySelector('#passwordLogin').value.trim();
+    const userT = document.querySelector('#userType').value;
+
+    if (email && password && userT) {
+        const response = await fetch('', {
+            method: 'POST',
+            body: JSON.stringify({ email, passowrd, userT}),
+            headers: {},
+        });
+        if (response.ok) {
+            document.location.replace('/');
+        } else {
+            alert('FAIL TRY AGAIN!');
+        }
+    }
+};
+
+const signiForm = async (event) => {
+    event.preventDefault();
+
+    const name = document.querySelector('#nameSignup').value.trim();
+    const email= document.querySelector('#emailSignup').value.trim();
+    const address= document.querySelector('#adrSignup').value.trim();
+    const dob= document.querySelector('#dobSignup').value.trim();
+    const phoneNum= document.querySelector('#phoneSignup').value.trim();
+    const password= document.querySelector('#passwordSignup').value.trim();
+    const userType= document.querySelector('#usrTypSignup').value;
+
+    if (name && email && address && dob && phoneNum && password && userType){
+        const response = await fetch('', {
+            method: 'POST',
+            body: JSON.stringify({ name, email, address, dob, phoneNum, password, userType}),
+            headers: {},
+        });
+        if (response.ok) {
+            document.location.replace('/');
+        } else {
+            alert('Failed to sign-up :(');
+        }
+    }
+};
+
+document
+    .querySelector('.login-form')
+    .addEventListener('submit', loginForm);
+
