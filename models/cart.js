0badbfff9e50c686c621aedcc98c490e629a47ee
Jennifer Vereecken <77600929+jbeans12@users.noreply.github.com>
2021-03-29 11:28:36 -0700

    cartModels2

--- models/cart.js
+++ models/cart.js
@@ -4,15 +4,36 @@ const sequelize = require('../config/connection');
 class Cart extends Model {}
 
 Cart.init(
-
+    {
+    id:{
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    products_id:{
+        type: DataTypes.INTEGER,
+        references: {
+            model: 'products',
+            key: 'id',
+        },
+    },
+    users_id:{
+        type: DataTypes.STRING,
+        references: {
+            model: 'users',
+            key: 'id',
+        },
+    }, 
+},
 {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
     modelName: 'cart',
-}
-)
+  }
+);
 
 module.exports = Cart
 
