802d99806c2ac9035cc460d153b88e864684945c
Jennifer Vereecken <77600929+jbeans12@users.noreply.github.com>
2021-03-29 11:54:59 -0700

    cartT

--- models/index.js
+++ models/index.js
@@ -2,14 +2,24 @@ const Users = require('./users')
 const Products = require('./products')
 const Cart = require('./cart')
 
+Products.belongsToMany(Users, {
+    foreignKey:'products_id',
+    through:{
+        model: Cart,
+        unique:false
+    },
+    as: 'users'
+});
+
+Users.hasMany(Products, {
+    foreignKey:'user_id',
+});
 
 
 
-
-
-
-
-
+// Products.belongsToMany(Users, {
+//     //?????????????? halp
+// });
 
 
 module.exports = { Users, Products, Cart }
