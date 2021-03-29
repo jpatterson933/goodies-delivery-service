e5df86ed95f107fcd4b88836411c427c62489474
Jennifer Vereecken <77600929+jbeans12@users.noreply.github.com>
2021-03-29 11:11:38 -0700

    cartModel

--- models/cart.js
+++ models/cart.js
@@ -1,3 +1,23 @@
-userid
-productid
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Cart extends Model {}
+
+
+Cart.init(
+{
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'cart',
+}
+)
+
+module.exports = Cart
+
+
+
+// userid
+// productid
 
