c3804db62d82364b9273f78513ec4961bcca22b1
Jeffery Patterson <jpatterson933@ucla.edu>
2021-03-28 23:08:10 -0700

    added 21 modale for age verification
    this does not contain the if statement and does not lead to tomatoe growing

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -3,6 +3,7 @@ const router = require('express').Router();
 
 router.get('/', async (req, res) => {
     res.render('homepage')
+    res.render('twentyone')
 })
 
 // router.get('/login', async (req, res) => {
