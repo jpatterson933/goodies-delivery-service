89f47cf9756dc17c656879aa42d699e14a6d9839
timmorse13 <timmorse13@gmail.com>
2021-03-29 12:05:07 -0700

    homepage hb

---
+++ public/js/homepage.js
@@ -0,0 +1,20 @@
+// const router = require('express').Router();
+const { Products } = require('../models');
+
+ for (let i = 0; i < Products.length; i ++) {
+     router.post('/products/:id', async (req, res) => {
+    try {
+
+        const productId = await Products.findByPk(req.params.id[i]);
+
+        const prodId = productId.get({ plain: true });
+        res.render('product',  prodId );
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+
+})
+ };
+
+    
