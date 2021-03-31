22a1f0ad3c4defa0723518f9d39600e57d8cb849
Jeffery Patterson <jpatterson933@ucla.edu>
2021-03-31 09:50:13 -0700

    added jaws

--- config/connection.js
+++ config/connection.js
@@ -2,23 +2,30 @@
 const Sequelize = require('sequelize');
 //we require dotenv so we can store our name, user and password in a separate file
 require('dotenv').config();
+if (process.env.JAWSDB_URL) {
+  const sequelize = new Sequelize(
+    process.env.JAWSDB_URL
+  )
+} else {
 
-const sequelize = new Sequelize(
-  //name of the database
-  process.env.DB_NAME,
-  //name of the user
-  process.env.DB_USER,
-  //your password
-  process.env.DB_PASSWORD,
-  {
-    //host for mysequal
-    host: 'localhost',
-    //the dialect configuration lets us know which database we are using
-    dialect: 'mysql',
-    //this is the port we are on in mysql
-    port: 3306,
-  }
-);
+
+  const sequelize = new Sequelize(
+    //name of the database
+    process.env.DB_NAME,
+    //name of the user
+    process.env.DB_USER,
+    //your password
+    process.env.DB_PASSWORD,
+    {
+      //host for mysequal
+      host: 'localhost',
+      //the dialect configuration lets us know which database we are using
+      dialect: 'mysql',
+      //this is the port we are on in mysql
+      port: 3306,
+    }
+  );
+}
 
 //once sequelize has been established, we want to export it
 module.exports = sequelize;
