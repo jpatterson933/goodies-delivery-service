c819f4c6bb185ed4d20def33c881c6ec0d0396dc
Jeffery Patterson <jpatterson933@ucla.edu>
2021-03-30 19:38:57 -0700

    more styling

--- public/js/slideshow.js
+++ public/js/slideshow.js
@@ -1,3 +1,4 @@
+
 var slideIndex = 0;
 showSlides();
 
@@ -10,5 +11,5 @@ function showSlides() {
   slideIndex++;
   if (slideIndex > slides.length) {slideIndex = 1}    
   slides[slideIndex-1].style.display = "block";  
-  setTimeout(showSlides, 2000); // Change image every 2 seconds
+  setTimeout(showSlides, 5000); // Change image every 2 seconds
 }
