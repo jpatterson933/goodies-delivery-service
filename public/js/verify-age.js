63e091f0a316ed534fb71dc5edb60b219066e14d
Jeffery Patterson <jpatterson933@ucla.edu>
2021-04-04 11:04:26 -0700

    Age verification temporarlily removed

--- public/js/verify-age.js
+++ public/js/verify-age.js
@@ -1,22 +1,24 @@
-const ofAge = $("#of-age");
-const notOfAge = $("#not-of-age");
-const modal = $(".overlay");
-const ageCheck = () => {
-    //this is our click event for our modal and what is ran when it appears.
-    ofAge.on("click", function(event) {
-        event.preventDefault();
-        localStorage.setItem("yes", "Twenty One")
-        modal.hide();
-    })
-    notOfAge.on("click", function() {
-        localStorage.setItem("no", "Not Twenty One")
-    })
-    const ageCheck = localStorage.getItem("yes");
-    const ageCheck2 = localStorage.getItem("no");
-    if (ageCheck) {
-        modal.hide()
-    } else if (ageCheck2) {
-        modal.show();
-    }
-}
-ageCheck();
+//--------COMMENTED OUT UNTIL FIGURE OUT A BETTER WAY TO HAVE IT POP UP----------//
+
+// const ofAge = $("#of-age");
+// const notOfAge = $("#not-of-age");
+// const modal = $(".overlay");
+// const ageCheck = () => {
+//     //this is our click event for our modal and what is ran when it appears.
+//     ofAge.on("click", function(event) {
+//         event.preventDefault();
+//         localStorage.setItem("yes", "Twenty One")
+//         modal.hide();
+//     })
+//     notOfAge.on("click", function() {
+//         localStorage.setItem("no", "Not Twenty One")
+//     })
+//     const ageCheck = localStorage.getItem("yes");
+//     const ageCheck2 = localStorage.getItem("no");
+//     if (ageCheck) {
+//         modal.hide()
+//     } else if (ageCheck2) {
+//         modal.show();
+//     }
+// }
+// ageCheck();
